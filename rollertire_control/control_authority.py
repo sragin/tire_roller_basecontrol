6d05ef64b0227252defd02c1983e891c2ee9bff5
Jongpil Kim <jpkim@koceti.re.kr>
2024-09-25 18:06:02 +0900

    first commit
    타이어롤러 제어프로그램 개발시작

---
+++ rollertire_control/control_authority.py
@@ -0,0 +1,130 @@
+# Copyright 2024 koceti_proprietary
+# All rights reserved.
+#
+# Unauthorized copying of this code base via any medium is strictly prohibited.
+# Proprietary and confidential.
+#
+# 타이어롤러 제어권한을 가져오기위한 노드
+import threading
+
+import can
+import rclpy
+from rclpy.node import Node
+
+
+class ControlAuthority(Node):
+
+    def __init__(self):
+        super().__init__('can_parser')
+        self.nodeName = self.get_name()
+        self.get_logger().info(f'{self.nodeName} started')
+
+        self.cnt = 0
+
+        self.bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=250000)
+        self.thread_canrecv = threading.Thread(target=self.work_canrecv)
+        self.thread_canrecv.daemon = True
+        self.thread_canrecv.start()
+
+    def work_canrecv(self):
+        while True:
+            try:
+                msg = self.bus.recv(timeout=1)
+                if msg is None:
+                    print('Timeout')
+                    pass
+                print(f'MSG: {msg}')
+                if msg.arbitration_id == 0x60B:
+                    print(f'{msg}')
+                    msg_58 = can.Message(
+                        arbitration_id=0x58B,
+                        is_extended_id=False,
+                        data=[0x60, 0x17, 0x10, 0, 0, 0, 0, 0])
+                    print(msg_58)
+                    self.bus.send(msg_58)
+                if msg.arbitration_id == 0x000:
+                    print(f'{msg}')
+                    msg_70 = can.Message(
+                        arbitration_id=0x70B,
+                        is_extended_id=False,
+                        data=[0x05])
+                    print(msg_70)
+                    self.bus.send(msg_70)
+                if msg.arbitration_id == 0x141:
+                    print(f'{msg}')
+                if msg.arbitration_id == 0x142:
+                    print(f'{msg}')
+                    self.send_lever_msg()
+            except AttributeError:
+                pass
+            except Exception as e:
+                print(f'recv error {e}')
+
+    def send_lever_msg(self):
+        try:
+            msg_101 = can.Message(
+                arbitration_id=0x101,
+                is_extended_id=False,
+                data=[0x00, 0x08, 0x00, 0xFF, 0x00, 0x00, 0x00, self.cnt])
+            self.cnt = self.cnt + 1
+            self.cnt = self.cnt % 256
+            msg_102 = can.Message(
+                arbitration_id=0x102,
+                is_extended_id=False,
+                data=[0, 0, 0, 0, 0, 0, 0, 0])
+            msg_102.data[0] = 255 - msg_101.data[0]
+            msg_102.data[1] = 255 - msg_101.data[1]
+            msg_102.data[2] = 255 - msg_101.data[2]
+            msg_102.data[3] = 255 - msg_101.data[3]
+            msg_102.data[4] = 255 - msg_101.data[4]
+            msg_102.data[5] = 255 - msg_101.data[5]
+            msg_102.data[6] = 255 - msg_101.data[6]
+            msg_102.data[7] = 255 - msg_101.data[7]
+            msg_103 = can.Message(
+                arbitration_id=0x103,
+                is_extended_id=False,
+                data=[0, 0, 0, 0, 0, 0, 0, msg_101.data[-1]])
+            msg_104 = can.Message(
+                arbitration_id=0x104,
+                is_extended_id=False,
+                data=[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, msg_102.data[-1]])
+            msg_105 = can.Message(
+                arbitration_id=0x105,
+                is_extended_id=False,
+                data=[0, 0x5C, 0x5F, 0, 0, 0, 0, msg_101.data[-1]])
+            msg_106 = can.Message(
+                arbitration_id=0x106,
+                is_extended_id=False,
+                data=[0xFF, 0xA3, 0xA0, 0xFF, 0xFF, 0xFF, 0xFF, msg_102.data[-1]])
+            msg_70 = can.Message(arbitration_id=0x70B, is_extended_id=False, data=[0x05])
+
+            self.bus.send(msg_70)
+            self.bus.send(msg_101)
+            self.bus.send(msg_102)
+            self.bus.send(msg_103)
+            self.bus.send(msg_104)
+            self.bus.send(msg_105)
+            self.bus.send(msg_106)
+
+            print(f'{msg_70}')
+            print(f'{msg_101}')
+            print(f'{msg_102}')
+            print(f'{msg_103}')
+            print(f'{msg_104}')
+            print(f'{msg_105}')
+            print(f'{msg_106}')
+        except Exception as e:
+            print(f'{e}')
+
+
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+        control = ControlAuthority()
+        rclpy.spin(control)
+    except KeyboardInterrupt as e:
+        print(e)
+    finally:
+        control.bus.shutdown()
+        if rclpy.ok():
+            control.destroy_node()
---
+++ setup.py
@@ -0,0 +1,38 @@
+from glob import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'rollertire_control'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('resource/*.dbc')),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='koceti',
+    maintainer_email='jpkim@koceti.re.kr',
+    description='Tire Roller Controller',
+    # license='koceti',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'control_authority = rollertire_control.control_authority:main',
+            # 'can_parser = rollertire_control.can_parser:main',
+            # 'can_sender = rollertire_control.can_sender:main',
+            # 'io_controller = rollertire_control.io_controller:main',
+            # 'bucket_controller = rollertire_control.bucket_controller:main',
+            # 'drive_controller = rollertire_control.drive_controller:main',
+            # 'navigator = rollertire_control.navigator:main',
+        ],
+    },
+)
