514efb0c0d03a0418a85b6452f3ab71a685a015d
Jongpil Kim <jpkim@koceti.re.kr>
2024-09-25 18:19:49 +0900

    fix: 누락된 파일 추가

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, glob('resource/*.dbc')),
         # Include all launch files.
         (os.path.join('share', package_name, 'launch'),
