247df2740c202c7f8294b3c0ebd05f88fdc48161
Jongpil Kim <jpkim@koceti.re.kr>
2025-04-29 19:10:15 +0900

    fix: 현재 송신하는 대시보드 신호값이 차량의 대시보드 신호값과 달라서 발생할 수 있는 오류 수정

--- tire_roller_basecontrol/can_sender.py
+++ tire_roller_basecontrol/can_sender.py
@@ -109,12 +109,14 @@ class CanSender(Node):
             id=0x105,
             is_extended=False,
             dlc=8,
-            data=[0, 0x36, 0x38, 0, 0, 0, 0, msg_101.data[-1]])
+            # data=[0, 0x36, 0x38, 0, 0, 0, 0, msg_101.data[-1]])
+            data=[0, 0x5d, 0x60, 0, 0, 0, 0, msg_101.data[-1]])
         msg_106 = Frame(
             id=0x106,
             is_extended=False,
             dlc=8,
-            data=[0xFF, 0xC9, 0xC7, 0xFF, 0xFF, 0xFF, 0xFF, msg_102.data[-1]])
+            # data=[0xFF, 0xC9, 0xC7, 0xFF, 0xFF, 0xFF, 0xFF, msg_102.data[-1]])
+            data=[0xFF, 0xa2, 0x9f, 0xFF, 0xFF, 0xFF, 0xFF, msg_102.data[-1]])
         msg_70 = Frame(
             id=0x70B,
             is_extended=False,
